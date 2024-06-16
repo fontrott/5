df4f07918da2d2e469a54cb978437ea0dbe7cdcb
fontrott <144059131+fontrott@users.noreply.github.com>
2024-06-16 20:41:09 +0300

    Add files via upload

---
+++ PR4/Bus.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR4
+{
+    public class Bus<T1, T2, T3, T4>
+    {
+        public T1 BusNumber { get; set; }
+        public T2 DriverNameAndSurname { get; set; }
+        public T3 RouteNumber { get; set; }
+        public T4 OnTheRoute { get; set; }
+
+        public Bus(T1 busNumber, T2 driverNameAndSurname, T3 routeNumber, T4 onTheRoute)
+        {
+            BusNumber = busNumber;
+            DriverNameAndSurname = driverNameAndSurname;
+            RouteNumber = routeNumber;
+            OnTheRoute = onTheRoute;
+        }
+
+        public delegate void AddBusDelegate(List<Bus<T1, T2, T3, T4>> buses, T1 busNumber, T2 driverNameAndSurname, T3 routeNumber, T4 onTheRoute);
+        public static AddBusDelegate AddNewBus;
+
+        public delegate void UpdateBusInfoDelegate(List<Bus<T1, T2, T3, T4>> buses, TextBox busesTextBox);
+        public static UpdateBusInfoDelegate UpdateBusInfo;
+        public void UpdateBusNumber(T1 BusNumber)
+        {
+            this.BusNumber = BusNumber;
+        }
+        public void UpdateDriverNameAndSurname(T2 DriverNameAndSurname)
+        {
+            this.DriverNameAndSurname = DriverNameAndSurname;
+        }
+        public void UpdateBusNumber(T3 RouteNumber)
+        {
+            this.RouteNumber = RouteNumber;
+        }
+        public void UpdateOnTheRoute(T4 OnTheRoute)
+        {
+            this.OnTheRoute = OnTheRoute;
+        }
+    }
+}
---
+++ PR4/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR4
+{
+    public partial class Buses : Form
+    {
+        List<Bus<int, string, string, bool>> bus = new List<Bus<int, string, string, bool>>();
+
+        public Buses()
+        {
+            InitializeComponent();
+        }
+        private void btn_InThePark_Click(object sender, EventArgs e)
+        {
+            txt_SearchBuses.Clear();
+            foreach (Bus<int, string, string, bool> b in bus)
+            {
+                if (b.OnTheRoute == false)
+                {
+                    string busInfo = $"Номер автобуса: {b.BusNumber}\r\nВодитель: {b.DriverNameAndSurname}\r\nМаршрут: {b.RouteNumber}\r\nСтатус: В парке";
+                    txt_SearchBuses.AppendText(busInfo + "\r\n\r\n");
+                }
+            }
+        }
+        private void btn_AddBus_Click(object sender, EventArgs e)
+        {
+            int busNumber;
+            if (!int.TryParse(txt_BusNumber.Text, out busNumber))
+            {
+                MessageBox.Show("Пожалуйста, введите корректный номер автобуса.");
+                return;
+            }
+
+            string driverName = txt_DriverNameAndSurname.Text;
+
+            bool isNewBus = chk_OnTheRoute.Checked;
+
+            string routeNumber = txt_RouteNumber.Text;
+
+            Bus<int, string, string, bool>.AddNewBus(bus, busNumber, driverName, routeNumber, isNewBus);
+
+            Bus<int, string, string, bool>.UpdateBusInfo(bus, buses_TextBox);
+
+            MessageBox.Show("Автобус успешно добавлен в список припаркованных автобусов.");
+        }
+
+        private void btn_Close1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Close2_Click(object sender, EventArgs e)
+        {
+            btn_Close1_Click(sender, e);
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_BusNumber.Clear();
+            txt_DriverNameAndSurname.Clear();
+            txt_RouteNumber.Clear();
+            txt_SearchBuses.Clear();
+            buses_TextBox.Clear();
+            bus.Clear();
+        }
+
+        private void btn_AddBus2_Click(object sender, EventArgs e)
+        {
+            btn_AddBus_Click(sender, e);
+        }
+
+        private void aboutTheProgram_Click(object sender, EventArgs e)
+        {
+            Form2 newF = new Form2();
+            newF.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bus<int, string, string, bool>.UpdateBusInfo(bus, buses_TextBox);
+        }
+
+        private void btn_OnTheRoute_Click(object sender, EventArgs e)
+        {
+            txt_SearchBuses.Clear();
+            foreach (Bus<int, string, string, bool> b in bus)
+            {
+                if (b.OnTheRoute == true)
+                {
+                    string busInfo = $"Номер автобуса: {b.BusNumber}\r\nВодитель: {b.DriverNameAndSurname}\r\nМаршрут: {b.RouteNumber}\r\nСтатус: На маршруте";
+                    txt_SearchBuses.AppendText(busInfo + "\r\n\r\n");
+                }
+            }
+        }
+    }
+}
